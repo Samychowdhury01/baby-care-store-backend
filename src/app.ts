ef16ee70de2a0db7d8ad0b54b7789dd01179badd
samychowdhury01 <samychowdhury84@gmail.com>
2024-09-10 11:48:40 +0600

    initial project setup complete with all important files

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// for global error
+app.use(globalErrorHandler);
+
+// for not found route
+app.use(notFound);
+
+export default app;
